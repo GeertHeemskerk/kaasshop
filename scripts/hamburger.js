7d2368a47246da96e43d4bd79872f3049103f682
GeertHeemskerk <leaguecrafterhd@gmail.com>
2018-10-01 21:15:07 +0200

    js menu done

--- scripts/hamburger.js
+++ scripts/hamburger.js
@@ -1,3 +1,15 @@
-document.getElementsByClassName('menuToggle')[0].onclick = function(){
-  var menu = document.getElementById('menu');
+document.getElementsByClassName('openMenu')[0].onclick = function(){
+  document.getElementsByClassName("openMenu")[0].style.visibility = "hidden";
+  document.getElementsByTagName("main")[0].style.visibility = "hidden";
+
+  document.getElementsByClassName("closeMenu")[0].style.visibility = "visible";
+  document.getElementById("menu").style.visibility = "visible";
+}
+
+document.getElementsByClassName('closeMenu')[0].onclick = function(){
+  document.getElementsByClassName("openMenu")[0].style.visibility = "visible";
+  document.getElementsByTagName("main")[0].style.visibility = "visible";
+
+  document.getElementsByClassName("closeMenu")[0].style.visibility = "hidden";
+  document.getElementById("menu").style.visibility = "hidden";
 }
